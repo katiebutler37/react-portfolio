8d2180adb208b412b8a05b2ef2dc216e19040328
Katie <katiebutler37@gmail.com>
2022-09-28 22:05:01 -0400

    navigation header styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import Navigation from '../Navigation';
+import React, {useState} from "react";
+import Navigation from "../Navigation";
+import { Icon } from 'react-icons-kit'
+import {menu} from 'react-icons-kit/feather/menu'
+import {x} from 'react-icons-kit/feather/x'
 
 function Header() {
+
+    const [toggle, setToggle] = useState(false);
+
+    const handleToggle=()=>{
+        setToggle(!toggle);
+    }
   return (
     <section>
+      <header className={toggle?'navbar expanded':'navbar'}>
+        <h2 className="page-name">
+          Katie Butler
+        </h2>
         <Navigation></Navigation>
+        <div className="toggle-icon" onClick={handleToggle}>
+            {toggle?<Icon icon={x} size={24}/>:<Icon icon={menu} size={24}/>}
+        </div>
+      </header>
     </section>
   );
 }
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Navigation() {
+    function itemSelected(e) {
+        switch(e.target.name) {
+            case ("about"):
+            break;
+            case ("portfolio"):
+            break;
+            case ("contact"):
+            break;
+            case ("resume"):
+            break;   
+            default:
+        }
+    };
+
+    
   return (
-    <section>
-    </section>
+    <nav>
+    <ul className='nav-items'>
+      <li>
+        <span className="nav-item" name="about" onClick={itemSelected}>About me</span>
+      </li>
+      <li>
+        <span className="nav-item" name="portfolio" onClick={itemSelected}>Portfolio</span>
+      </li>
+        <li>
+          <span className="nav-item"  name="contact" onClick={itemSelected}>Contact</span>
+        </li> 
+        <li>
+          <span className="nav-item" name="resume" onClick={itemSelected}>Resume</span>
+        </li>
+    </ul>
+  </nav>
   );
 }
 
